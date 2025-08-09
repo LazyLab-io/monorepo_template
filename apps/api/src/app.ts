e6a96ca099acd36a691cb336fe9b4f24d76289d5
O.R <orezek@seznam.cz>
2025-08-09 13:50:30 +0200

    build: prepare api template - add host

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -6,7 +6,7 @@ fastify.get("/", function (request, reply) {
   reply.send({ hello: "world" });
 });
 
-fastify.listen({host: "0.0.0.0", port: 3000 }, function (err) {
+fastify.listen({ host: "0.0.0.0", port: 3000 }, function (err) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
